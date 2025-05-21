baef5422a4275decbb62c8c6b001a28441f8cfcd
Diego Cantalapiedra <source@cantalaweb.com>
2025-05-21 21:38:28 +0200

    Added swagger.js file and model files.

---
+++ server/models/poll.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose;
+
+const pollSchema = new Schema({
+    polls: [
+        date: { type: Date, default: Date.now },
+        users: [
+            userName: {type: String, required: true},
+            userIP: {type: String, required: true},
+            chosenOption: {type: String, required: true}
+        ]
+    ]
+});
+  
+const Poll = mongoose.model('Poll', pollSchema);
+
+module.exports = { Poll }
---
+++ server/models/question.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose;
+
+const questionSchema = new Schema({
+    question: {type: String, required: true},
+    minsToVote: {type: Number, default: 2},
+    options: [
+        text: {type: String, required: true}
+    ],
+    createdAt: { type: Date, default: Date.now },
+});
+  
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = { Question }
