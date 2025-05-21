beb8c525488b5e9c279ff85ca325e13c7be93e88
Diego Cantalapiedra <source@cantalaweb.com>
2025-05-21 22:22:14 +0200

    Fixed objects inside arrays in model files.

--- server/models/question.model.js
+++ server/models/question.model.js
@@ -4,9 +4,9 @@ const { Schema } = mongoose;
 const questionSchema = new Schema({
     question: {type: String, required: true},
     minsToVote: {type: Number, default: 2},
-    options: [
-        text: {type: String, required: true}
-    ],
+    options: [{
+        text: {type: String, required: true},
+    }],
     createdAt: { type: Date, default: Date.now },
 });
   
