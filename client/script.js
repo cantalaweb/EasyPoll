e560cd97a4eecefaefe56c0773ee03d5a1a5c336
Diego Cantalapiedra <source@cantalaweb.com>
2025-05-22 20:36:24 +0200

    Added client/script.js

--- client/script.js
+++ client/script.js
@@ -1,1 +1,42 @@
-console.log("hola")
+// Get DOM elements
+const questionText = document.getElementById('questionText');
+const minsToVoteText = document.getElementById('minsToVoteText');
+const option1 = document.getElementById('option_1');
+const option2 = document.getElementById('option_2');
+const option3 = document.getElementById('option_3');
+const option4 = document.getElementById('option_4');
+const createPollBtn = document.getElementById('createPollBtn');
+const BASEURL = 'http://127.0.0.1:8000'
+
+createPollBtn.addEventListener('click', createPoll)
+
+const createPoll = async () => {
+    const response = await fetch(BASEURL + "/questions", {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            question: questionText.value,
+            minsToVote: Number(minsToVoteText.value)
+            options: [
+                option1.value,
+                option2.value,
+                option3.value,
+                option4.value
+            ],
+        }),
+    })    
+    try {
+        const newId = await response.json()
+        questionText.value = ""
+        minsToVoteText.value = "2"
+        option1.value = ""
+        option2.value = ""
+        option3.value = ""
+        option4.value = ""
+    } catch (error) {
+        console.error(error);
+        window.alert('Question could not be created.')
+    }
+}
